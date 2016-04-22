925d453af4c10edc8ba25fdcd76d2f79754e4e4b
Oliver Heim <oliver.heim@iwelt.de>
2016-04-22 20:10:25 +0200

    application class

---
+++ app/src/main/java/com/obstacle3/app/App.java
@@ -0,0 +1,12 @@
+package com.obstacle3.app;
+
+import android.app.Application;
+
+import org.androidannotations.annotations.EApplication;
+
+/**
+ * Created by oliverheim on 22.04.16.
+ */
+@EApplication
+public class App extends Application {
+}
